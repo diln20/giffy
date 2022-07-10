2b7c39934af02f63e7d49ab72f9afbe0e5af7a7f
diln20 <andreytorres418@gmail.com>
2022-07-09 19:13:55 -0500

    V2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  
+  const GIFS=['https://media1.giphy.com/media/nVsLCrW5iHf6E/giphy.webp?cid=ecf05e47qmexj7k34qfgu6i2skjb34kbwortc1ovmuvyppzd&rid=giphy.webp&ct=g'
+  ,'https://media4.giphy.com/media/26ybvMeJmCaPtcNDW/200w.webp?cid=ecf05e47qmexj7k34qfgu6i2skjb34kbwortc1ovmuvyppzd&rid=200w.webp&ct=g'
+  ]
+  const [gifs,setGifs]= useState(GIFS)
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+      {
+      gifs.map(
+          singleGif=>
+            <img src={singleGif} alt=""/>
+        )
+      }
+      </section>
     </div>
   );
 }
